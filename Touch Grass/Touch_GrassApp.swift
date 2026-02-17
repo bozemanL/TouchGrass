9f0232da2ae76e88401da8b5d53793d8e9add21c
Lillian Bozeman <lillianbozeman@gmail.com>
2026-02-17 15:07:42 -0800

    Initial Commit

---
+++ Touch Grass/Touch_GrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Touch_GrassApp.swift
+//  Touch Grass
+//
+//  Created by Lily Bozeman on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Touch_GrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Touch GrassTests/Touch_GrassTests.swift
@@ -0,0 +1,16 @@
+//
+//  Touch_GrassTests.swift
+//  Touch GrassTests
+//
+//  Created by Lily Bozeman on 2/17/26.
+//
+
+import Testing
+
+struct Touch_GrassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
