4a9ad444af0d43ee37132698be687d55fad249a2
Lillian Bozeman <lillianbozeman@gmail.com>
2026-03-18 12:13:17 -0700

    Add comments to the contentView page detailing what is going on.

--- Touch Grass/ContentView.swift
+++ Touch Grass/ContentView.swift
@@ -13,35 +13,39 @@ struct ContentView: View {
     }
 }
 
+// This is a view that contains all of our forecast display.
 struct ForecastView: View {
+    // State variables to hold our current forecast.
     @State private var forecast: ForecastPeriod? = nil
     @State private var location: LocationInfo? = nil
     
     var body: some View {
         VStack {
+            
+            // If the location of the forecast exists, show the location.
             if let location = location {
                 Text("\(location.city), \(location.state)")
             }
             
             
-            
+            // If the forecast exists, display forecast information.
             if let forecast = forecast {
-                Text(forecast.name)
-                    .font(.largeTitle)
-                Text("High: \(forecast.temperature)°\(forecast.temperatureUnit)")
-                Text(forecast.shortForecast)
-                
-                
-                
-                
+                // This is an icon retrieved from NOAA api.
                 AsyncImage (url: URL(string: forecast.icon)) {
                     image in image
                         .resizable()
                         .scaledToFit()
                 } placeholder: {
                     ProgressView()
-                }.frame(width: 100, height: 100)
+                }
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
                 
+                // Forecast information
+                Text(forecast.name)
+                    .font(.largeTitle)
+                Text("High: \(forecast.temperature)°\(forecast.temperatureUnit)")
+                Text(forecast.shortForecast)
             }
         } .task {
             // Get the location.
